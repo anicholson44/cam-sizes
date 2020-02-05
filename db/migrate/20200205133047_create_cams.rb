88ba8a11793164252faca03e291bec144a6dda58
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2020-02-05 13:48:28 -0500

    rubocop

--- db/migrate/20200205133047_create_cams.rb
+++ db/migrate/20200205133047_create_cams.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateCams < ActiveRecord::Migration[6.0]
   def change
     create_table :cams do |t|
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,32 +12,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_133047) do
-
+ActiveRecord::Schema.define(version: 20_200_205_133_047) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "cam_styles", force: :cascade do |t|
-    t.string "name", null: false
-    t.bigint "manufacturer_id", null: false
-    t.index ["manufacturer_id"], name: "index_cam_styles_on_manufacturer_id"
+  create_table 'cam_styles', force: :cascade do |t|
+    t.string 'name', null: false
+    t.bigint 'manufacturer_id', null: false
+    t.index ['manufacturer_id'], name: 'index_cam_styles_on_manufacturer_id'
   end
 
-  create_table "cams", force: :cascade do |t|
-    t.string "name", null: false
-    t.bigint "cam_style_id", null: false
-    t.decimal "range_min", null: false
-    t.decimal "range_max", null: false
-    t.decimal "weight", null: false
-    t.decimal "strength", null: false
-    t.string "color", null: false
-    t.index ["cam_style_id"], name: "index_cams_on_cam_style_id"
+  create_table 'cams', force: :cascade do |t|
+    t.string 'name', null: false
+    t.bigint 'cam_style_id', null: false
+    t.decimal 'range_min', null: false
+    t.decimal 'range_max', null: false
+    t.decimal 'weight', null: false
+    t.decimal 'strength', null: false
+    t.string 'color', null: false
+    t.index ['cam_style_id'], name: 'index_cams_on_cam_style_id'
   end
 
-  create_table "manufacturers", force: :cascade do |t|
-    t.string "name", null: false
+  create_table 'manufacturers', force: :cascade do |t|
+    t.string 'name', null: false
   end
 
-  add_foreign_key "cam_styles", "manufacturers"
-  add_foreign_key "cams", "cam_styles"
+  add_foreign_key 'cam_styles', 'manufacturers'
+  add_foreign_key 'cams', 'cam_styles'
 end
