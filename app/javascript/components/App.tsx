4cdbdfd1ba7081f81eb1329d94e6a4035c22865c
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2020-02-05 14:28:41 -0500

    add semantic-ui-react loader

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,16 +1,30 @@
 import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
+import { Loader } from 'semantic-ui-react';
 
 import store, { actions } from '../store';
+import { RootState } from '../store/types';
+
+import 'semantic-ui-css/semantic.min.css';
+
+const AppContainer = () => {
+    const loading = useSelector<RootState>(({ loading }) => loading);
+    
+    return (
+        <div id='container'>
+            <Loader active={!!loading} size='big' />
+        </div>
+    );
+};
 
 const App = () => {
     useEffect(() => {
         store.dispatch(actions.fetchCamsAsync.request())
     });
-    
+
     return (
         <Provider store={store}>
-            <div></div>
+            <AppContainer />
         </Provider>
     );
 };
