b3819c206d8d77d9d6b0898658e65337320fdb7c
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2020-02-06 19:10:31 -0500

    pad cam rects

--- app/javascript/components/CamChart/CamRect.tsx
+++ app/javascript/components/CamChart/CamRect.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const height = 15;
+const padding = 1;
 
 const CamRect = ({ color, stroke, x, width, index }: {color: string, stroke: string, x: number, width: number, index: number }) => (
-    <rect fill={color} stroke={stroke} x={x} width={width} y={index * height} height={height} />
+    <rect fill={color} stroke={stroke} x={x} width={width} y={index * (height + padding)} height={height} />
 );
 
 export default CamRect;
