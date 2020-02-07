cb4d1a9af3e664b6e2d30b952395291f7595a9fc
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2020-02-07 12:39:11 -0500

    show cams under cam style in accordion

--- app/javascript/components/CamMenu/ManufacturerMenuItem.tsx
+++ app/javascript/components/CamMenu/ManufacturerMenuItem.tsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { List } from "semantic-ui-react";
-import { RootState, CamStyle } from "../../store";
+import { Accordion, Menu, List } from "semantic-ui-react";
+import { RootState, CamStyle, EntityMap, Cam, IdStore } from "../../store";
 import CamStyleCheckbox from "./CamStyleCheckbox";
 
 const ManufacturerMenuItem = ({ id }: { id: number }) => {
   const camStyles = useSelector<RootState, CamStyle[]>(({ entities }) =>
     entities.manufacturers[id].camStyles.map(id => entities.camStyles[id])
   );
+  const cams = useSelector<RootState, EntityMap<Cam>>(
+    ({ entities }) => entities.cams
+  );
+  const [activeAccordions, setActiveAccordions] = useState<IdStore>({});
 
   return (
-    <List>
-      {camStyles.map((camStyle) => (
-        <List.Item key={camStyle.id}><CamStyleCheckbox {...camStyle}/></List.Item>
-      ))}
-    </List>
+    <Accordion as={Menu} vertical>
+      {camStyles.map(camStyle => {
+        const active = activeAccordions[camStyle.id];
+        return (
+          <Menu.Item key={camStyle.id}>
+            <Accordion.Title
+              active={active}
+              content={<CamStyleCheckbox {...camStyle} />}
+              onClick={() => {
+                const newActiveAccordions = { ...activeAccordions };
+                if (active) {
+                  delete newActiveAccordions[camStyle.id];
+                } else {
+                  newActiveAccordions[camStyle.id] = true;
+                }
+                setActiveAccordions(newActiveAccordions);
+              }}
+            />
+            <Accordion.Content
+              active={active}
+              content={
+                <List>
+                  {camStyle.cams.map(id => (
+                    <List.Item key={id}>{cams[id].name} {cams[id].color}</List.Item>
+                  ))}
+                </List>
+              }
+            />
+          </Menu.Item>
+        );
+      })}
+    </Accordion>
   );
 };
 
